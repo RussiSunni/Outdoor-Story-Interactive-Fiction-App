d621172e4568493029df9353e05eb09c11dc14d3
RussiSunni <jonathandyason@hotmail.com>
2023-06-26 12:49:36 +0200

    loading screen.

--- public/js/town/scene7.js
+++ public/js/town/scene7.js
@@ -13,6 +13,50 @@ export default class Scene7 extends Phaser.Scene {
         this.load.image("trail-2", "assets/Backgrounds/Town/trail-2.jpg");
         this.load.image("gramps", "assets/Characters/gramps.png");
         this.load.image("trail-split", "assets/Backgrounds/Town/trail-split.jpg");
+
+        // Loading screen.
+        var progressBar = this.add.graphics();
+        var progressBox = this.add.graphics();
+        progressBox.fillStyle(0x222222, 0.8);
+        progressBox.fillRect(240, 270, 320, 50);
+
+        var width = this.cameras.main.width;
+        var height = this.cameras.main.height;
+        var loadingText = this.make.text({
+            x: width / 2,
+            y: height / 2 - 50,
+            text: 'Loading...',
+            style: {
+                font: '20px monospace',
+                fill: '#ffffff'
+            }
+        });
+        loadingText.setOrigin(0.5, 0.5);
+
+        var percentText = this.make.text({
+            x: width / 2,
+            y: height / 2 - 5,
+            text: '0%',
+            style: {
+                font: '18px monospace',
+                fill: '#ffffff'
+            }
+        });
+        percentText.setOrigin(0.5, 0.5);
+
+        this.load.on('progress', function (value) {
+            percentText.setText(parseInt(value * 100) + '%');
+            progressBar.clear();
+            progressBar.fillStyle(0xffffff, 1);
+            progressBar.fillRect(250, 280, 300 * value, 30);
+        });
+
+        this.load.on('complete', function () {
+            progressBar.destroy();
+            progressBox.destroy();
+            loadingText.destroy();
+            percentText.destroy();
+        });
     }
 
     // data is for carrying across the music variable.
